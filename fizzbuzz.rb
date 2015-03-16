9b1de5f82c321dd92755f0556de6914c3cd44c1b
Jordan Hollinger <jordan.hollinger@gmail.com>
2015-03-16 19:44:04 -0400

    Performance improvement

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,10 @@
 def fizzbuzz(input)
   input.map do |n|
-    if n % 3 == 0 and n % 5 == 0
-      'FizzBuzz'
-    elsif n % 3 == 0
-      'Fizz'
-    elsif n % 5 == 0
-      'Buzz'
-    else
-      n.to_s
-    end
-  end
+    result = ''
+    result << 'Fizz' if n % 3 == 0
+    result << 'Buzz' if n % 5 == 0
+    result << n.to_s if result.size == 0
+    result
 end
 
 if $0 == __FILE__
