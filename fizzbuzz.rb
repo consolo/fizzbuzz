892333bf58f955f834f62a87ef90882fbb4e373c
Jeff McArdle <jmcardle@consoloservices.com>
2015-03-17 15:22:17 -0400

    the ruby master

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,14 @@
 def fizzbuzz(input)
   input.map do |n|
-    n.to_s
+    if n % 5 == 0 && n % 3 == 0
+      "FizzBuzz"
+    elsif n % 5 == 0
+      "Buzz"
+    elsif n % 3 == 0
+      "Fizz"
+    else
+      n.to_s
+    end
   end
 end
 
